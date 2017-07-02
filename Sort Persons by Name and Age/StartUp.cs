319f2836c4e97410d5635557aa819e43aa01313e
Mario Slavov <hellsflash@gmail.com>
2017-07-02 07:22:23 +0300

    Add project files.

---
+++ Sort Persons by Name and Age/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace  Sort_Salary_Validation_and_Team
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var inputNum = int.Parse(Console.ReadLine());
+            var firstTeam =new Team("firstTeam");
+            var reserveTeam = new Team("reserveTeam");
+            for (int i = 0; i < inputNum; i++)
+            {
+                var inputArgs = Console.ReadLine().Split();
+                firstTeam.AddPlayer(new Person(inputArgs[0],inputArgs[1],int.Parse(inputArgs[2]),double.Parse(inputArgs[3])));
+                reserveTeam.AddPlayer(new Person(inputArgs[0], inputArgs[1], int.Parse(inputArgs[2]), double.Parse(inputArgs[3])));
+            }
+
+            Console.WriteLine($"First team have { firstTeam.FirstTeam.Count} players");
+            Console.WriteLine($"Reserve team have {reserveTeam.ReserveTeam.Count} players");
+        }
+
+    }
+}
